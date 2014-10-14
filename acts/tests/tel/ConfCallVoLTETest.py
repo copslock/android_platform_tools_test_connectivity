991bc7fdfdd4b53dc909905452b8041d52164915
Yang Liu <yangxliu@google.com>
2014-10-14 10:05:28 -0700

    Major change in Tel test cases. Split ConfCall test, add Stress Call test.
    Add stress call CSFB(CS fall back) test.
    Add stress call 3G test.
    Add stress call VoLTE test.
    Split ConfCall test into ConfCall and ConfCallVoLTE.
    Make the phone number check logic more soft.
    If number is in US number format, the '+1' or '1' ahead of number will be ignored.
    Add API to get operator name.
    In sanity test cases, some steps will not run for Vzw DUT.
    
    Change-Id: Ifc71597a41b99b89bc8c6def13a372d1fe53348b

--- acts/tests/tel/ConfCallVoLTETest.py
+++ acts/tests/tel/ConfCallVoLTETest.py
@@ -14,13 +14,14 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 """
-    Test Script for Telephony Conference Call
+    Test Script for Telephony Conference Call VoLTE
 """
 
 import time
 from base_test import BaseTestClass
 from queue import Empty
 from test_utils.tel.tel_test_utils import call_process
+from test_utils.tel.tel_test_utils import get_network_type
 from test_utils.tel.tel_test_utils import get_operator_name
 from test_utils.tel.tel_test_utils import get_phone_number
 from test_utils.tel.tel_test_utils import set_preferred_network_type
@@ -32,8 +33,8 @@ from test_utils.tel.tel_test_utils import verify_incall_state
 from test_utils.tel.tel_test_utils import wait_for_droid_in_network_type
 from test_utils.utils import load_config
 
-class ConfCallTest(BaseTestClass):
-    TAG = "ConfCallTest"
+class ConfCallVoLTETest(BaseTestClass):
+    TAG = "ConfCallVoLTETest"
     log_path = ''.join((BaseTestClass.log_path, TAG, "/"))
 
     def __init__(self, controllers):
@@ -45,12 +46,12 @@ class ConfCallTest(BaseTestClass):
         self.droid2, self.ed2 = self.android_devices[2].get_droid()
         self.ed2.start()
         self.tests = (
-                      "test_conf_call",
+                      "test_conf_call_volte",
                       )
         # The path for "sim config file" should be set
         # in "testbed.config" entry "sim_conf_file".
         self.simconf = load_config(self.user_params["sim_conf_file"])
-        self.time_wait_in_call = 15
+        self.time_wait_in_call = 30
         self.delay_between_test = 5
         self.max_wait_time_for_lte = 120
 
@@ -61,13 +62,13 @@ class ConfCallTest(BaseTestClass):
             setattr(self,"phone_number_" + str(i), num)
             self.log.info("phone_number_{} : {} <{}>".format(i, num,
                                                              get_operator_name(d)))
-        for droid in [self.droid0, self.droid1, self.droid2]:
-            if droid.imsIsEnhanced4gLteModeSettingEnabledByPlatform():
-                toggle_volte(droid, False)
-        return True
-
-    def teardown_class(self):
-        for droid in [self.droid0, self.droid1, self.droid2]:
+        # Only droid0 and droid1 required to be LTE
+        for (droid, ed) in [(self.droid0, self.ed0), (self.droid1, self.ed1)]:
+            if get_network_type(droid, "data") != "lte":
+                set_preferred_network_type(droid, "LTE")
+                toggle_airplane_mode(self.log, droid, ed, True)
+                toggle_airplane_mode(self.log, droid, ed, False)
+        for droid in [self.droid0, self.droid1]:
             if droid.imsIsEnhanced4gLteModeSettingEnabledByPlatform():
                 toggle_volte(droid, True)
         return True
@@ -82,35 +83,54 @@ class ConfCallTest(BaseTestClass):
         return True
 
     """ Tests Begin """
-    def test_conf_call(self):
-        """ Test Conf Call among three phones.
+    def test_conf_call_volte(self):
+        """Test Conf Call VoLTE case among three phones.
 
+        Make Sure phone is on LTE mode.
         Call from PhoneA to PhoneB, accept on PhoneB.
         Call from PhoneA to PhoneC, accept on PhoneC.
         On PhoneA, merge to conference call.
-        End call on PhoneC, verify call continues.
-        Call from PhoneC to PhoneA, accept on PhoneA.
-        On PhoneA, merge to conference call.
-        End call between A and C on PhoneA, verify call continues.
+        End call on PhoneB, verify call continues.
+        Disable VoLTE on PhoneB.
+        Call from PhoneB to PhoneC, accept on PhoneC.
+        On PhoneC, merge to conference call.
+        End call on PhoneA, verify call continues.
 
         Returns:
             True if pass; False if fail.
         """
         # TODO(yangxliu): Add code to check voice. Currently only check status.
-        toggle_airplane_mode(self.log, self.droid0, self.ed0, False)
-        toggle_airplane_mode(self.log, self.droid1, self.ed1, False)
-        toggle_airplane_mode(self.log, self.droid2, self.ed2, False)
+        # Check if VoLTE is enabled by platform before proceed this test case.
+        # PhoneA and PhoneB are required to have VoLTE.
+        # PhoneC is OK to use non-VoLTE phone.
+        for droid in [self.droid0, self.droid1]:
+            if not droid.imsIsEnhanced4gLteModeSettingEnabledByPlatform():
+                self.log.error("VoLTE is not supported by platform on {}.".
+                               format(droid))
+                return False
+        # Wait for droids in LTE mode, before proceed VoLTE call.
+        wait_for_droid_in_network_type(self.log,
+                                       [self.droid0, self.droid1],
+                                       self.max_wait_time_for_lte, "lte")
         self.log.info("Step1: Call From PhoneA to PhoneB.")
         call_process(self.log, self.droid0, self.droid1,
                      self.ed0, self.ed1, self.time_wait_in_call,
-                     self.phone_number_0, self.phone_number_1)
+                     self.phone_number_0, self.phone_number_1,
+                     verify_call_mode_caller = True,
+                     verify_call_mode_callee = True,
+                     caller_mode_VoLTE = True,
+                     callee_mode_VoLTE = True)
         verify_active_call_number(self.droid0, 1)
         calls = self.droid0.telecomPhoneGetCallIds()
         call_one_id = calls[0]
         self.log.info("Step2: Call From PhoneA to PhoneC.")
         call_process(self.log, self.droid0, self.droid2,
                      self.ed0, self.ed2, self.time_wait_in_call,
-                     self.phone_number_0, self.phone_number_2)
+                     self.phone_number_0, self.phone_number_2,
+                     verify_call_mode_caller = True,
+                     verify_call_mode_callee = False,
+                     caller_mode_VoLTE = True,
+                     callee_mode_VoLTE = False)
         verify_active_call_number(self.droid0, 2)
         calls = self.droid0.telecomPhoneGetCallIds()
         call_two_id = calls[1]
@@ -120,39 +140,59 @@ class ConfCallTest(BaseTestClass):
         verify_active_call_number(self.droid0, 3)
         droids = [self.droid0, self.droid1, self.droid2]
         verify_incall_state(self.log, droids, True)
-        self.log.info("Step4: End call on PhoneC and verify call continues.")
-        self.droid2.telecomEndCall()
+        self.log.info("Step4: End call on PhoneB and verify call continues.")
+        self.droid1.telecomEndCall()
         time.sleep(self.time_wait_in_call)
-        verify_active_call_number(self.droid0, 1)
-        droids = [self.droid0, self.droid1]
-        verify_incall_state(self.log, droids, True)
-        droids = [self.droid2]
-        verify_incall_state(self.log, droids, False)
-        call_AB = self.droid0.telecomPhoneGetCallIds()[0]
-        self.log.info("Step5: Call from PhoneC to PhoneA.")
-        call_process(self.log, self.droid2, self.droid0,
-                     self.ed2, self.ed0, self.time_wait_in_call,
-                     self.phone_number_2, self.phone_number_0)
-        verify_active_call_number(self.droid0, 2)
-        self.log.info("Step6: Merge to Conf Call and verify Conf Call.")
-        call_AC = None
+        # In Non-VoLTE Conf call, we need to :
+        # verify_active_call_number(self.droid0, 1).
+        # However, in VoLTE Conf Call, the number of calls in Conf host is still
+        # 3 at this point.
+        # So in here print the calls in host as log.info.
         calls = self.droid0.telecomPhoneGetCallIds()
-        for call in calls:
-            if call != call_AB:
-                call_AC = call
-        self.droid0.telecomJoinCallsInConf(call_AB, call_AC)
+        self.log.info("Current active calls in Conf host: {}. Total: {}".
+                      format(calls, len(calls)))
+        droids = [self.droid0, self.droid2]
+        verify_incall_state(self.log, droids, True)
+        droids = [self.droid1]
+        verify_incall_state(self.log, droids, False)
+        self.log.info("Step5: Disable VoLTE PhoneB, Call from PhoneB to PhoneC.")
+        toggle_volte(self.droid1, False)
+        # Wait for disable volte to take effect in android system.
+        time.sleep(5)
+        # Make sure internet connection still valid at this time.
+        verify_http_connection(self.droid1)
+        call_process(self.log, self.droid1, self.droid2,
+                     self.ed1, self.ed2, self.time_wait_in_call,
+                     self.phone_number_1, self.phone_number_2,
+                     verify_call_mode_caller = True,
+                     verify_call_mode_callee = False,
+                     caller_mode_VoLTE = False,
+                     callee_mode_VoLTE = False)
+        verify_active_call_number(self.droid2, 2)
+        self.log.info("Step6: Merge to Conf Call and verify Conf Call.")
+        calls = self.droid2.telecomPhoneGetCallIds()
+        call_one_id = calls[0]
+        call_two_id = calls[1]
+        self.droid2.telecomJoinCallsInConf(call_one_id, call_two_id)
         time.sleep(self.time_wait_in_call)
-        verify_active_call_number(self.droid0, 3)
+        verify_active_call_number(self.droid2, 3)
         droids = [self.droid0, self.droid1, self.droid2]
         verify_incall_state(self.log, droids, True)
-        self.log.info("Step7: End from A to C and verify call continues")
-        self.droid0.telecomCallDisconnect(call_AC)
-        time.sleep(self.time_wait_in_call)
-        verify_active_call_number(self.droid0, 1)
-        droids = [self.droid0, self.droid1]
-        verify_incall_state(self.log, droids, True)
-        droids = [self.droid2]
-        verify_incall_state(self.log, droids, False)
+        self.log.info("Step7: End call on A, and verify call continues")
         self.droid0.telecomEndCall()
+        time.sleep(self.time_wait_in_call)
+        verify_active_call_number(self.droid2, 1)
+        droids = [self.droid1, self.droid2]
+        verify_incall_state(self.log, droids, True)
+        droids = [self.droid0]
+        verify_incall_state(self.log, droids, False)
+        self.droid2.telecomEndCall()
+        for droid in [self.droid0, self.droid1, self.droid2]:
+            if droid.imsIsEnhanced4gLteModeSettingEnabledByPlatform():
+                toggle_volte(droid, True)
+        time.sleep(5)
+        verify_active_call_number(self.droid0, 0)
+        verify_active_call_number(self.droid1, 0)
+        verify_active_call_number(self.droid2, 0)
         return True
     """ Tests End """
