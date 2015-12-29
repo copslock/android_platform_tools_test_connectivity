1bdf77dbbe1b13737b7380dc0a525cc970e7e764
tturney <tturney@google.com>
2015-12-28 17:46:13 -0800

    Update Copyright to 2016 and cleanup
    Remove vim comments
    s/\/usr\/bin\/python3.4/\/usr\/bin\/env python3\.4/g
    
    Change-Id: I0cde4f2a6989d6df91cf560fc6ef511c7ad405ff

--- acts/framework/acts/jsonrpc.py
+++ acts/framework/acts/jsonrpc.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3.4
-# vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
-
-#   Copyright 2014- Google, Inc.
+#!/usr/bin/env python3.4
+#
+#   Copyright 2016- Google, Inc.
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
