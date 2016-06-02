82522819e12f48a4763d59b07a7868ac1ed4f24c
Ang Li <angli@google.com>
2016-06-02 13:57:21 -0700

    Update wifi test util with better styles and to utilize new ACTS features.
    Add a retry option for wifi_connect.
    
    Resolve any descrepancy in Wi-Fi code between internal master and
    AOSP.
    
    Bug=27821528
    Bug=28342613
    
    Change-Id: I77dc14cb5d6f72c4a538745e7acf0013702009d6

--- acts/tests/google/wifi/WifiManagerTest.py
+++ acts/tests/google/wifi/WifiManagerTest.py
@@ -28,23 +28,18 @@ from acts import asserts
 WifiEnums = wutils.WifiEnums
 WifiEventNames = wutils.WifiEventNames
 
-class WifiManagerTest(acts.base_test.BaseTestClass):
 
+class WifiManagerTest(acts.base_test.BaseTestClass):
     def setup_class(self):
         self.dut = self.android_devices[0]
         wutils.wifi_test_device_init(self.dut)
-        req_params = (
-            "iot_networks",
-            "open_network",
-            "iperf_server_address",
-            "tdls_models",
-            "energy_info_models"
-            )
+        req_params = ("iot_networks", "open_network", "iperf_server_address",
+                      "tdls_models", "energy_info_models")
         self.unpack_userparams(req_params)
-        asserts.assert_true(len(self.iot_networks) > 0,
+        asserts.assert_true(
+            len(self.iot_networks) > 0,
             "Need at least one iot network with psk.")
-        asserts.assert_true(wutils.wifi_toggle_state(self.dut, True),
-            "Failed to turn on wifi before tests.")
+        wutils.wifi_toggle_state(self.dut, True)
         self.iot_networks = self.iot_networks + [self.open_network]
         self.iperf_server = self.iperf_servers[0]
 
@@ -63,6 +58,7 @@ class WifiManagerTest(acts.base_test.BaseTestClass):
         self.dut.cat_adb_log(test_name, begin_time)
 
     """Helper Functions"""
+
     def connect_to_wifi_network_with_password(self, params):
         """Connection logic for open and psk wifi networks.
 
@@ -76,62 +72,51 @@ class WifiManagerTest(acts.base_test.BaseTestClass):
         Returns:
             True if successful, False otherwise.
         """
-        result = False
         wait_time = 5
         network, ad = params
         droid = ad.droid
         ed = ad.ed
         SSID = network[WifiEnums.SSID_KEY]
-        try:
-            ed.clear_all_events()
-            wutils.start_wifi_connection_scan(ad)
-            droid.wifiStartTrackingStateChange()
-            asserts.assert_true(droid.wifiConnect(network),
-                "wifi connect returned false.")
-            connect_result = ed.pop_event(WifiEventNames.WIFI_CONNECTED)
-            self.log.debug(connect_result)
-            result = connect_result['data'][WifiEnums.SSID_KEY] == SSID
-            if result:
-                self.log.info("Starting iperf traffic through {}".format(SSID))
-                time.sleep(wait_time)
-                port_arg = "-p {}".format(self.iperf_server.port)
-                result, data = ad.run_iperf_client(self.iperf_server_address,
-                                                   port_arg)
-                self.log.debug(pprint.pformat(data))
-        except queue.Empty:
-            self.log.exception("Failed to connect to {}".format(SSID))
-        finally:
-            droid.wifiStopTrackingStateChange()
-        return result
+        ed.clear_all_events()
+        wutils.start_wifi_connection_scan(ad)
+        scan_results = droid.wifiGetScanResults()
+        wutils.assert_network_in_list({WifiEnums.SSID_KEY: SSID}, scan_results)
+        wutils.wifi_connect(ad, network, num_of_tries=3)
+        self.log.info("Starting iperf traffic through {}".format(SSID))
+        time.sleep(wait_time)
+        port_arg = "-p {}".format(self.iperf_server.port)
+        success, data = ad.run_iperf_client(self.iperf_server_address,
+                                            port_arg)
+        self.log.debug(pprint.pformat(data))
+        asserts.assert_true(success, "Error occurred in iPerf traffic.")
 
     def run_iperf(self, iperf_args):
         if "iperf_server_address" not in self.user_params:
             self.log.error(("Missing iperf_server_address. "
-                "Provide one in config."))
+                            "Provide one in config."))
         else:
             iperf_addr = self.user_params["iperf_server_address"]
             self.log.info("Running iperf client.")
-            result, data = self.dut.run_iperf_client(iperf_addr,
-                iperf_args)
+            result, data = self.dut.run_iperf_client(iperf_addr, iperf_args)
             self.log.debug(data)
 
     def run_iperf_rx_tx(self, time, omit=10):
         args = "-p {} -t {} -O 10".format(self.iperf_server.port, time, omit)
         self.log.info("Running iperf client {}".format(args))
         self.run_iperf(args)
-        args = "-p {} -t {} -O 10 -R".format(self.iperf_server.port, time, omit)
+        args = "-p {} -t {} -O 10 -R".format(self.iperf_server.port, time,
+                                             omit)
         self.log.info("Running iperf client {}".format(args))
         self.run_iperf(args)
 
     """Tests"""
+
     def test_toggle_state(self):
         """Test toggling wifi"""
         self.log.debug("Going from on to off.")
-        asserts.assert_true(wutils.wifi_toggle_state(self.dut, False),
-                         "Failed to turn wifi off.")
+        wutils.wifi_toggle_state(self.dut, False)
         self.log.debug("Going from off to on.")
-        asserts.assert_true(wutils.wifi_toggle_state(self.dut, True),
-                         "Failed to turn wifi on.")
+        wutils.wifi_toggle_state(self.dut, True)
 
     def test_toggle_with_screen(self):
         """Test toggling wifi with screen on/off"""
@@ -142,12 +127,10 @@ class WifiManagerTest(acts.base_test.BaseTestClass):
         time.sleep(wait_time)
         self.log.debug("Going from on to off.")
         try:
-            asserts.assert_true(wutils.wifi_toggle_state(self.dut, False),
-                             "Failed to turn wifi off.")
+            wutils.wifi_toggle_state(self.dut, False)
             time.sleep(wait_time)
             self.log.debug("Going from off to on.")
-            asserts.assert_true(wutils.wifi_toggle_state(self.dut, True),
-                             "Failed to turn wifi on.")
+            wutils.wifi_toggle_state(self.dut, True)
         finally:
             self.dut.droid.wakeLockRelease()
             time.sleep(wait_time)
@@ -159,11 +142,9 @@ class WifiManagerTest(acts.base_test.BaseTestClass):
         self.log.debug("Start regular wifi scan.")
         wutils.start_wifi_connection_scan(self.dut)
         wifi_results = self.dut.droid.wifiGetScanResults()
-        self.log.debug("Scan results: %s" % wifi_results)
+        self.log.debug("Scan results: %s", wifi_results)
         ssid = self.open_network[WifiEnums.SSID_KEY]
-        condition = {WifiEnums.SSID_KEY: ssid}
-        asserts.assert_true(wutils.match_networks(condition, wifi_results),
-                         "Can not find expected network %s" % ssid)
+        wutils.assert_network_in_list({WifiEnums.SSID_KEY: ssid}, wifi_results)
 
     def test_add_network(self):
         """Test wifi connection scan."""
@@ -173,10 +154,8 @@ class WifiManagerTest(acts.base_test.BaseTestClass):
         configured_networks = self.dut.droid.wifiGetConfiguredNetworks()
         self.log.debug(("Configured networks after adding: %s" %
                         configured_networks))
-        condition = {WifiEnums.SSID_KEY: ssid}
-        asserts.assert_true(wutils.match_networks(condition, configured_networks),
-                         ("Could not find expected network %s in configured "
-                          "networks.") % ssid)
+        wutils.assert_network_in_list(
+            {WifiEnums.SSID_KEY: ssid}, configured_networks)
 
     def test_forget_network(self):
         self.test_add_network()
@@ -184,13 +163,15 @@ class WifiManagerTest(acts.base_test.BaseTestClass):
         wutils.wifi_forget_network(self.dut, ssid)
         configured_networks = self.dut.droid.wifiGetConfiguredNetworks()
         for nw in configured_networks:
-            asserts.assert_true(nw[WifiEnums.BSSID_KEY] != ssid,
+            asserts.assert_true(
+                nw[WifiEnums.BSSID_KEY] != ssid,
                 "Found forgotten network %s in configured networks." % ssid)
 
     @acts.signals.generated_test
     def test_iot_with_password(self):
-        params = list(itertools.product(self.iot_networks, self.android_devices))
-        name_gen = lambda p : "test_connection_to-%s" % p[0][WifiEnums.SSID_KEY]
+        params = list(itertools.product(self.iot_networks,
+                                        self.android_devices))
+        name_gen = lambda p: "test_connection_to-%s" % p[0][WifiEnums.SSID_KEY]
         failed = self.run_generated_testcases(
             self.connect_to_wifi_network_with_password,
             params,
@@ -201,13 +182,13 @@ class WifiManagerTest(acts.base_test.BaseTestClass):
         model = acts.utils.trim_model_name(self.dut.model)
         self.log.debug("Model is %s" % model)
         if model in self.tdls_models:
-            asserts.assert_true(self.dut.droid.wifiIsTdlsSupported(),
-                             ("TDLS should be supported on %s, but device is "
-                              "reporting not supported.") % model)
+            asserts.assert_true(self.dut.droid.wifiIsTdlsSupported(), (
+                "TDLS should be supported on %s, but device is "
+                "reporting not supported.") % model)
         else:
-            asserts.assert_true(not self.dut.droid.wifiIsTdlsSupported(),
-                             ("TDLS should not be supported on %s, but device "
-                              "is reporting supported.") % model)
+            asserts.assert_true(not self.dut.droid.wifiIsTdlsSupported(), (
+                "TDLS should not be supported on %s, but device "
+                "is reporting supported.") % model)
 
     def test_energy_info(self):
         """Verify the WiFi energy info reporting feature.
@@ -227,11 +208,12 @@ class WifiManagerTest(acts.base_test.BaseTestClass):
         model = self.dut.model
         expected_support = model in self.energy_info_models
         msg = "Expect energy info support to be %s on %s, got %s." % (
-              expected_support, model, actual_support)
+            expected_support, model, actual_support)
         asserts.assert_true(actual_support == expected_support, msg)
         if not actual_support:
-            asserts.skip(("Device %s does not support energy info reporting as "
-                       "expected.") % model)
+            asserts.skip(
+                ("Device %s does not support energy info reporting as "
+                 "expected.") % model)
         # Verify reported values don't decrease.
         self.log.info(("Device %s supports energy info reporting, verify that "
                        "the reported values don't decrease.") % model)
@@ -243,12 +225,12 @@ class WifiManagerTest(acts.base_test.BaseTestClass):
             new_energy = info["ControllerEnergyUsed"]
             new_idle_time = info["ControllerIdleTimeMillis"]
             asserts.assert_true(new_energy >= energy,
-                "Energy value decreased: previous %d, now %d" % (energy,
-                    new_energy))
+                                "Energy value decreased: previous %d, now %d" %
+                                (energy, new_energy))
             energy = new_energy
             asserts.assert_true(new_idle_time >= idle_time,
-                "Idle time decreased: previous %d, now %d" % (idle_time,
-                    new_idle_time))
+                                "Idle time decreased: previous %d, now %d" % (
+                                    idle_time, new_idle_time))
             idle_time = new_idle_time
             wutils.start_wifi_connection_scan(self.dut)
 
