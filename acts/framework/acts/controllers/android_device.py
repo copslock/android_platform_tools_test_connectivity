f3ffd929ed8ec9e4546bb83282b26b94304364b7
Tom Turney <tturney@google.com>
2017-06-20 19:32:38 +0000

    Revert "Re-organize logs for easy bug reporting."
    This reverts commit 2b46d89a6ed1b8afd5183e2ec8450f125b166570.
    
    Change-Id: I711170939d44c5f57bb34dff9cf82a1b58c5393c
    (cherry picked from commit 07efec1a7e61ea5ff340da0aa1c322ef2d5452d0)

--- acts/framework/acts/controllers/android_device.py
+++ acts/framework/acts/controllers/android_device.py
@@ -539,7 +539,7 @@ class AndroidDevice:
 
     @property
     def is_adb_logcat_on(self):
-        """Whether the adb logcat process is active.
+        """Whether there is an ongoing adb logcat collection.
         """
         if self.adb_logcat_process:
             try:
@@ -552,8 +552,9 @@ class AndroidDevice:
             if ret is None:
                 return True
             else:
-                self.log.error(
-                    'Logcat process %s died' % self.adb_logcat_process)
+                if self.droid:
+                    self.droid.logI('Logcat died')
+                self.log.error('Logcat died on %s' % self.adb_logcat_file_path)
                 return False
         return False
 
@@ -719,7 +720,7 @@ class AndroidDevice:
                         if in_range:
                             break
 
-    def start_adb_logcat(self, cont_logcat_file=False, logcat_file_path=None):
+    def start_adb_logcat(self, cont_logcat_file=False):
         """Starts a standing adb logcat collection in separate subprocesses and
         save the logcat in a file.
 
@@ -728,32 +729,41 @@ class AndroidDevice:
                               file.  This allows for start_adb_logcat to act
                               as a restart logcat function if it is noticed
                               logcat is no longer running.
-            log_cat_file_path: the file path to collect logcat.
         """
+        if self.is_adb_logcat_on:
+            raise AndroidDeviceError(("Android device {} already has an adb "
+                                      "logcat thread going on. Cannot start "
+                                      "another one.").format(self.serial))
         # Disable adb log spam filter. Have to stop and clear settings first
         # because 'start' doesn't support --clear option before Android N.
-        if self.adb_logcat_process and self.is_adb_logcat_on:
-            self.stop_adb_logcat()
         self.adb.shell("logpersist.stop --clear")
         self.adb.shell("logpersist.start")
-        if not logcat_file_path:
-            logcat_file_path = self.log_path
-        utils.create_dir(logcat_file_path)
-        logcat_output_file = os.path.join(
-            logcat_file_path, "adblog_%s_%s.txt" % (self.model, self.serial))
-        extra_params = getattr(self, "adb_logcat_param", "-b all")
-        cmd = "adb -s %s logcat -v threadtime %s" % (self.serial, extra_params)
-        redirect = ">>" if cont_logcat_file else ">"
-        cmd = " ".join([cmd, redirect, logcat_output_file])
+        if cont_logcat_file:
+            if self.droid:
+                self.droid.logI('Restarting logcat')
+            self.log.warning(
+                'Restarting logcat on file %s' % self.adb_logcat_file_path)
+            logcat_file_path = self.adb_logcat_file_path
+        else:
+            f_name = "adblog,{},{}.txt".format(self.model, self.serial)
+            utils.create_dir(self.log_path)
+            logcat_file_path = os.path.join(self.log_path, f_name)
+        try:
+            extra_params = self.adb_logcat_param
+        except AttributeError:
+            extra_params = "-b all"
+        cmd = "adb -s {} logcat -v threadtime {} >> {}".format(
+            self.serial, extra_params, logcat_file_path)
         self.adb_logcat_process = utils.start_standing_subprocess(cmd)
-        self.adb_logcat_file_path = logcat_output_file
+        self.adb_logcat_file_path = logcat_file_path
 
     def stop_adb_logcat(self):
         """Stops the adb logcat collection subprocess.
         """
-        if not self.adb_logcat_process:
-            self.log.info(
-                "Device does not have an ongoing adb logcat collector.")
+        if not self.is_adb_logcat_on:
+            raise AndroidDeviceError(
+                "Android device %s does not have an ongoing adb logcat collection."
+                % self.serial)
         utils.stop_standing_subprocess(self.adb_logcat_process)
         self.adb_logcat_process = None
 
@@ -847,7 +857,7 @@ class AndroidDevice:
                 new_br = False
         except adb.AdbError:
             new_br = False
-        br_path = self.log_path
+        br_path = os.path.join(self.log_path, test_name)
         utils.create_dir(br_path)
         out_name = "AndroidDevice%s_%s" % (self.serial, begin_time.replace(
             " ", "_").replace(":", "-"))
@@ -882,12 +892,11 @@ class AndroidDevice:
         """Pull files from devies."""
         if not remote_path:
             remote_path = self.log_path
-        utils.create_dir(remote_path)
         for file_name in files:
             self.adb.pull(
                 "%s %s" % (file_name, remote_path), timeout=PULL_TIMEOUT)
 
-    def check_crash_report(self, log_crash_report=False):
+    def check_crash_report(self, log_crash_report=False, test_name=None):
         """check crash report on the device."""
         crash_reports = []
         for crash_path in CRASH_REPORT_PATHS:
@@ -896,7 +905,11 @@ class AndroidDevice:
                     continue
                 crash_reports.append(os.path.join(crash_path, report))
         if log_crash_report:
-            crash_log_path = os.path.join(self.log_path, "crashes")
+            if not test_name:
+                test_name = time.strftime("%m-%d-%Y-%H-%M-%S")
+            crash_log_path = os.path.join(self.log_path, test_name,
+                                          "CrashReports")
+            utils.create_dir(crash_log_path)
             self.pull_files(crash_reports, crash_log_path)
         return crash_reports
 
