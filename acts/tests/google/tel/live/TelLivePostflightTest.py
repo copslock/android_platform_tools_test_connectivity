98513d780f04774f8ef279e717a5eff14de1fec6
Betty Zhou <bettyzhou@google.com>
2017-11-02 17:33:36 -0700

    Simplify the TelLivePostflightTest.
    Test: run ACTS test
    Bug: None
    Change-Id: I7e6113b1ff3ab7b424d29c165d8bd408f335df3e

--- acts/tests/google/tel/live/TelLivePostflightTest.py
+++ acts/tests/google/tel/live/TelLivePostflightTest.py
@@ -18,12 +18,31 @@
 """
 import os
 from acts import utils
+from acts.asserts import fail
+from acts.base_test import BaseTestClass
 from acts.test_decorators import test_tracker_info
 from acts.test_utils.tel.TelephonyBaseTest import TelephonyBaseTest
-from acts.asserts import fail
 
 
 class TelLivePostflightTest(TelephonyBaseTest):
+    def __init__(self, controllers):
+        BaseTestClass.__init__(self, controllers)
+
+    def setup_class(self):
+        pass
+
+    def teardown_class(self):
+        pass
+
+    def setup_test(self):
+        pass
+
+    def on_pass(self, *arg):
+        pass
+
+    def on_fail(self, *arg):
+        pass
+
     @test_tracker_info(uuid="ba6e260e-d2e1-4c01-9d51-ef2df1591039")
     @TelephonyBaseTest.tel_test_wrap
     def test_check_crash(self):
@@ -33,7 +52,8 @@ class TelLivePostflightTest(TelephonyBaseTest):
             pre_crash = getattr(ad, "crash_report_preflight", [])
             crash_diff = list(set(post_crash).difference(set(pre_crash)))
             if crash_diff:
-                msg += "%s find new crash reports %s" % (ad.serial, crash_diff)
+                msg += "%s find new crash reports %s " % (ad.serial,
+                                                          crash_diff)
                 ad.log.error("Find new crash reports %s", crash_diff)
                 crash_path = os.path.join(ad.log_path, self.test_id, "Crashes")
                 utils.create_dir(crash_path)
