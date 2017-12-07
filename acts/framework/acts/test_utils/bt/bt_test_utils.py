15e4b92baf95ea1e34f69cd281fae0908022438a
Stanley Tng <stng@google.com>
2017-12-06 16:32:41 -0800

    Fix more pre-commit format errors
    The change is automatically done by the yapf scripts.
    
    Test: None but code is reviewed for no logic changes
    Bug: None
    Change-Id: I900ef4aaf2694e6626f4220e9594542ac2480019

--- acts/framework/acts/test_utils/bt/bt_test_utils.py
+++ acts/framework/acts/test_utils/bt/bt_test_utils.py
@@ -230,7 +230,7 @@ def setup_multiple_devices_for_bt_test(android_devices):
             d = a.droid
             # TODO: Create specific RPC command to instantiate
             # BluetoothConnectionFacade. This is just a workaround.
-            d.bluetoothStartConnectionStateChangeMonitor("");
+            d.bluetoothStartConnectionStateChangeMonitor("")
             setup_result = d.bluetoothSetLocalName(generate_id_by_size(4))
             if not setup_result:
                 a.log.error("Failed to set device name.")
