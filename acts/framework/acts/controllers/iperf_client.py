7fc44d396510d840c8d67abbb1791c3125e28ac1
globaledge <globaledgeofficial@gmail.com>
2018-02-20 21:37:11 +0530

    Make iperf client as controller.
    This change will help to run iperf client on remote machine.
    
    Change-Id: I01fafccb95ff27a0f69acf6d51a9bbc655f7c752

--- acts/framework/acts/controllers/iperf_client.py
+++ acts/framework/acts/controllers/iperf_client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-#   Copyright 2017 - The Android Open Source Project
+#   Copyright 2018 - The Android Open Source Project
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -16,46 +16,90 @@
 """
 Starts iperf client on host machine.
 """
-from acts import utils
 import os
+import logging
 import subprocess
+
+from acts import utils
+from acts.controllers.utils_lib.ssh import connection
+from acts.controllers.utils_lib.ssh import settings
+
+ACTS_CONTROLLER_CONFIG_NAME = "IPerfClient"
+ACTS_CONTROLLER_REFERENCE_NAME = "iperf_clients"
+
+
+def create(configs):
+    """ Factory method for iperf client.
+
+    The function creates iperf clients based on at least one config.
+    If configs only specify a port number, a regular local IPerfClient object
+    will be created. If configs contains ssh settings for a remote host,
+    a RemoteIPerfClient object will be instantiated
+
+    Args:
+        config: config parameters for the iperf client
+    """
+    results = []
+    print(configs)
+    for c in configs:
+        try:
+            results.append(IPerfClient(c))
+        except:
+            pass
+    return results
+
+
+def destroy(objs):
+    for ipf in objs:
+        try:
+            ipf.stop()
+        except:
+            pass
 
 
 class IPerfClient():
     """Class that handles iperf3 client operations."""
 
-    def __init__(self, port, ip_address, log_path):
-        self.port = port
-        self.log_path = os.path.join(log_path, "iPerf{}".format(self.port))
-        self.iperf_cmd = "iperf3 -c {} -i1".format(ip_address)
+    def __init__(self, config):
+        if type(config) is dict and "ssh_config" in config:
+            self.ssh_settings = settings.from_config(config["ssh_config"])
+            self.ssh_session = connection.SshConnection(self.ssh_settings)
+            self.client_type = "remote"
+        else:
+            self.client_type = config["type"]
         self.iperf_process = None
         self.log_files = []
-        self.started = False
 
-    def start(self, extra_args="", tag=""):
-        """Starts iperf client on specified port on host machine.
+    def start(self, iperf_args, ad, ip):
+        """Starts iperf client on specified port.
 
         Args:
-            extra_args: A string representing extra arguments to start iperf
-            client with.
-            tag: Appended to log file name to identify logs from different
-            iperf runs.
+            iperf_args: A string representing arguments to start iperf
+            client. Eg: iperf_args = "-t 10 -p 5001 -w 512k/-u -b 200M -J".
+            ad: Android device object.
+            ip: Iperf server ip address.
 
         Returns:
             full_out_path: Iperf result path.
         """
-        if self.started:
-            return
-        utils.create_dir(self.log_path)
-        if tag:
-            tag = tag + ','
-        out_file_name = "IPerfClient,{},{}{}.log".format(
-            self.port, tag, len(self.log_files))
-        full_out_path = os.path.join(self.log_path, out_file_name)
-        cmd = "{} {}".format(self.iperf_cmd, extra_args)
-        cmd = cmd.split()
-        with open(full_out_path, "w") as f:
-            subprocess.call(cmd, stdout=f)
+        iperf_cmd = "iperf3 -c {} ".format(ip) + iperf_args
+        port = iperf_cmd.split(' ')[6]
+        log_path = os.path.join(ad.log_path, "iPerf{}".format(port))
+        utils.create_dir(log_path)
+        out_file_name = "IPerfClient,{},{}.log".format(port, len(
+            self.log_files))
+        full_out_path = os.path.join(log_path, out_file_name)
+        if self.client_type == "remote":
+            try:
+                job_result = self.ssh_session.run(iperf_cmd)
+                self.iperf_process = job_result.stdout
+                with open(full_out_path, 'w') as outfile:
+                    outfile.write(self.iperf_process)
+            except Exception:
+                logging.info("Iperf run failed.")
+        else:
+            cmd = iperf_cmd.split()
+            with open(full_out_path, "w") as f:
+                subprocess.call(cmd, stdout=f)
         self.log_files.append(full_out_path)
-        self.started = True
         return full_out_path
