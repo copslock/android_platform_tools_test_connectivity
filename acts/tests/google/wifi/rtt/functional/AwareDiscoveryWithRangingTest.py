cc821572b94b423c257e75f6bbab47345402f00e
Etan Cohen <etancohen@google.com>
2018-04-10 14:13:38 -0700

    [AWARE][RTT] Add UUIDs to tests
    Integrate tests into test tracking infrastructure.
    
    Bug: 76006054
    Bug: 76008963
    Test: AttachTest, DataPathTest, DiscoveryTest, AwareDiscoveryWithRangingTest
    Change-Id: I5b1b92323003c82e16ae9e0414efa6ba2ae4c4a5

--- acts/tests/google/wifi/rtt/functional/AwareDiscoveryWithRangingTest.py
+++ acts/tests/google/wifi/rtt/functional/AwareDiscoveryWithRangingTest.py
@@ -18,6 +18,7 @@ import sys
 import time
 
 from acts import asserts
+from acts.test_decorators import test_tracker_info
 from acts.test_utils.wifi.aware import aware_const as aconsts
 from acts.test_utils.wifi.aware import aware_test_utils as autils
 from acts.test_utils.wifi.aware.AwareBaseTest import AwareBaseTest
@@ -201,6 +202,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
   # <ref_distance>: inrange or outoforange
   #########################################################################
 
+  @test_tracker_info(uuid="3a216e9a-7a57-4741-89c0-84456975e1ac")
   def test_ranged_discovery_unsolicited_passive_prange_snorange(self):
     """Verify discovery with ranging:
     - Unsolicited Publish/Passive Subscribe
@@ -221,6 +223,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=False)
 
+  @test_tracker_info(uuid="859a321e-18e2-437b-aa7a-2a45a42ee737")
   def test_ranged_discovery_solicited_active_prange_snorange(self):
     """Verify discovery with ranging:
     - Solicited Publish/Active Subscribe
@@ -241,6 +244,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=False)
 
+  @test_tracker_info(uuid="12a4f899-4f70-4641-8f3c-351004669b71")
   def test_ranged_discovery_unsolicited_passive_pnorange_smax_inrange(self):
     """Verify discovery with ranging:
     - Unsolicited Publish/Passive Subscribe
@@ -265,6 +269,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=False)
 
+  @test_tracker_info(uuid="b7f90793-113d-4355-be20-856d92ac939f")
   def test_ranged_discovery_solicited_active_pnorange_smax_inrange(self):
     """Verify discovery with ranging:
     - Solicited Publish/Active Subscribe
@@ -289,6 +294,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=False)
 
+  @test_tracker_info(uuid="da3ab6df-58f9-44ae-b7be-8200d9e1bb76")
   def test_ranged_discovery_unsolicited_passive_pnorange_smin_outofrange(self):
     """Verify discovery with ranging:
     - Unsolicited Publish/Passive Subscribe
@@ -313,6 +319,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=False)
 
+  @test_tracker_info(uuid="275e0806-f266-4fa6-9ca0-1cfd7b65a6ca")
   def test_ranged_discovery_solicited_active_pnorange_smin_outofrange(self):
     """Verify discovery with ranging:
     - Solicited Publish/Active Subscribe
@@ -337,6 +344,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=False)
 
+  @test_tracker_info(uuid="8cd0aa1e-6866-4a5d-a550-f25483eebea1")
   def test_ranged_discovery_unsolicited_passive_prange_smin_inrange(self):
     """Verify discovery with ranging:
     - Unsolicited Publish/Passive Subscribe
@@ -360,6 +368,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=True)
 
+  @test_tracker_info(uuid="97c22c54-669b-4f7a-bf51-2f484e5f3e74")
   def test_ranged_discovery_unsolicited_passive_prange_smax_inrange(self):
     """Verify discovery with ranging:
     - Unsolicited Publish/Passive Subscribe
@@ -383,6 +392,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=True)
 
+  @test_tracker_info(uuid="616673d7-9d0b-43de-a378-e5e949b51b32")
   def test_ranged_discovery_unsolicited_passive_prange_sminmax_inrange(self):
     """Verify discovery with ranging:
     - Unsolicited Publish/Passive Subscribe
@@ -407,6 +417,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=True)
 
+  @test_tracker_info(uuid="2bf84912-dcad-4a8f-971f-e445a07f05ce")
   def test_ranged_discovery_solicited_active_prange_smin_inrange(self):
     """Verify discovery with ranging:
     - Solicited Publish/Active Subscribe
@@ -430,6 +441,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=True)
 
+  @test_tracker_info(uuid="5cfd7961-9665-4742-a1b5-2d1fc97f9795")
   def test_ranged_discovery_solicited_active_prange_smax_inrange(self):
     """Verify discovery with ranging:
     - Solicited Publish/Active Subscribe
@@ -453,6 +465,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=True)
 
+  @test_tracker_info(uuid="5cf650ad-0b42-4b7d-9e05-d5f45fe0554d")
   def test_ranged_discovery_solicited_active_prange_sminmax_inrange(self):
     """Verify discovery with ranging:
     - Solicited Publish/Active Subscribe
@@ -477,6 +490,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=True)
 
+  @test_tracker_info(uuid="5277f418-ac35-43ce-9b30-3c895272898e")
   def test_ranged_discovery_unsolicited_passive_prange_smin_outofrange(self):
     """Verify discovery with ranging:
     - Unsolicited Publish/Passive Subscribe
@@ -499,6 +513,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
             max_distance_mm=None),
         expect_discovery=False)
 
+  @test_tracker_info(uuid="8a7e6ab1-acf4-41a7-a5fb-8c164d593b5f")
   def test_ranged_discovery_unsolicited_passive_prange_smax_outofrange(self):
     """Verify discovery with ranging:
     - Unsolicited Publish/Passive Subscribe
@@ -521,6 +536,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
             max_distance_mm=0),
         expect_discovery=False)
 
+  @test_tracker_info(uuid="b744f5f9-2641-4373-bf86-3752e2f9aace")
   def test_ranged_discovery_unsolicited_passive_prange_sminmax_outofrange(self):
     """Verify discovery with ranging:
     - Unsolicited Publish/Passive Subscribe
@@ -544,6 +560,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
             max_distance_mm=1000001),
         expect_discovery=False)
 
+  @test_tracker_info(uuid="d2e94199-b2e6-4fa5-a347-24594883c801")
   def test_ranged_discovery_solicited_active_prange_smin_outofrange(self):
     """Verify discovery with ranging:
     - Solicited Publish/Active Subscribe
@@ -566,6 +583,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
             max_distance_mm=None),
         expect_discovery=False)
 
+  @test_tracker_info(uuid="a5619835-496a-4244-a428-f85cba3d4115")
   def test_ranged_discovery_solicited_active_prange_smax_outofrange(self):
     """Verify discovery with ranging:
     - Solicited Publish/Active Subscribe
@@ -588,6 +606,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
             max_distance_mm=0),
         expect_discovery=False)
 
+  @test_tracker_info(uuid="12ebd91f-a973-410b-8ee1-0bd86024b921")
   def test_ranged_discovery_solicited_active_prange_sminmax_outofrange(self):
     """Verify discovery with ranging:
     - Solicited Publish/Active Subscribe
@@ -622,6 +641,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
   # <scenario>: test scenario (details in name)
   #########################################################################
 
+  @test_tracker_info(uuid="59442180-4a6c-428f-b926-86000e8339b4")
   def test_ranged_updated_discovery_unsolicited_passive_oor_to_ir(self):
     """Verify discovery with ranging operation with updated configuration:
     - Unsolicited Publish/Passive Subscribe
@@ -647,6 +667,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=True)
 
+  @test_tracker_info(uuid="60188508-104d-42d5-ac3a-3605093c45d7")
   def test_ranged_updated_discovery_unsolicited_passive_pub_unrange(self):
     """Verify discovery with ranging operation with updated configuration:
     - Unsolicited Publish/Passive Subscribe
@@ -667,6 +688,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=False)
 
+  @test_tracker_info(uuid="f96b434e-751d-4eb5-ae01-0c5c3a6fb4a2")
   def test_ranged_updated_discovery_unsolicited_passive_sub_unrange(self):
     """Verify discovery with ranging operation with updated configuration:
     - Unsolicited Publish/Passive Subscribe
@@ -688,6 +710,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=False)
 
+  @test_tracker_info(uuid="78970de8-9362-4647-931a-3513bcf58e80")
   def test_ranged_updated_discovery_unsolicited_passive_sub_oor(self):
     """Verify discovery with ranging operation with updated configuration:
     - Unsolicited Publish/Passive Subscribe
@@ -711,6 +734,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
             max_distance_mm=100001),
         expect_discovery=False)
 
+  @test_tracker_info(uuid="0841ad05-4899-4521-bd24-04a8e2e345ac")
   def test_ranged_updated_discovery_unsolicited_passive_pub_same(self):
     """Verify discovery with ranging operation with updated configuration:
     - Unsolicited Publish/Passive Subscribe
@@ -732,6 +756,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         s_config=None, # no updates
         expect_discovery=False)
 
+  @test_tracker_info(uuid="ec6ca57b-f115-4516-813a-4572b930c8d3")
   def test_ranged_updated_discovery_unsolicited_passive_multi_step(self):
     """Verify discovery with ranging operation with updated configuration:
     - Unsolicited Publish/Passive Subscribe
@@ -776,6 +801,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
             expect_discovery=True,
             expect_range=False)
 
+  @test_tracker_info(uuid="bbaac63b-000c-415f-bf19-0906f04031cd")
   def test_ranged_updated_discovery_solicited_active_oor_to_ir(self):
     """Verify discovery with ranging operation with updated configuration:
     - Solicited Publish/Active Subscribe
@@ -801,6 +827,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=True)
 
+  @test_tracker_info(uuid="c385b361-7955-4f34-9109-8d8ca81cb4cc")
   def test_ranged_updated_discovery_solicited_active_pub_unrange(self):
     """Verify discovery with ranging operation with updated configuration:
     - Solicited Publish/Active Subscribe
@@ -821,6 +848,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=False)
 
+  @test_tracker_info(uuid="ec5120ea-77ec-48c6-8820-48b82ad3dfd4")
   def test_ranged_updated_discovery_solicited_active_sub_unrange(self):
     """Verify discovery with ranging operation with updated configuration:
     - Solicited Publish/Active Subscribe
@@ -842,6 +870,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         expect_discovery=True,
         expect_range=False)
 
+  @test_tracker_info(uuid="6231cb42-91e4-48d3-b9db-b37efbe8537c")
   def test_ranged_updated_discovery_solicited_active_sub_oor(self):
     """Verify discovery with ranging operation with updated configuration:
     - Solicited Publish/Active Subscribe
@@ -865,6 +894,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
             max_distance_mm=100001),
         expect_discovery=False)
 
+  @test_tracker_info(uuid="ec999420-6a50-455e-b624-f4c9b4cb7ea5")
   def test_ranged_updated_discovery_solicited_active_pub_same(self):
     """Verify discovery with ranging operation with updated configuration:
     - Solicited Publish/Active Subscribe
@@ -886,6 +916,7 @@ class AwareDiscoveryWithRangingTest(AwareBaseTest, RttBaseTest):
         s_config=None, # no updates
         expect_discovery=False)
 
+  @test_tracker_info(uuid="ec6ca57b-f115-4516-813a-4572b930c8d3")
   def test_ranged_updated_discovery_solicited_active_multi_step(self):
     """Verify discovery with ranging operation with updated configuration:
     - Unsolicited Publish/Passive Subscribe
