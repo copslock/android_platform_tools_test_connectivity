739d6b77d5812ba9ec86e644351aa45c3d81fb0d
Betty Zhou <bettyzhou@google.com>
2018-04-18 10:38:53 -0700

    Add allow_non_emergency_calls_in_ecm_bool test.
    Add 3G/2G/IWLAN locked SIM test cases.
    
    Add 3G/2G no SIM test cases.
    
    Fix qxdm log path for a fixed giving mask path.
    
    Test: run ACTS test
    Bug: None
    Change-Id: If56fa9999b39ace737921a492d6da763237a2d7d

--- acts/tests/google/tel/live/TelLiveLockedSimTest.py
+++ acts/tests/google/tel/live/TelLiveLockedSimTest.py
@@ -21,15 +21,27 @@ import time
 from acts import signals
 from acts.test_decorators import test_tracker_info
 from acts.test_utils.tel.TelephonyBaseTest import TelephonyBaseTest
+from acts.test_utils.tel.tel_defines import CAPABILITY_WFC
 from acts.test_utils.tel.tel_defines import DEFAULT_DEVICE_PASSWORD
+from acts.test_utils.tel.tel_defines import GEN_2G
+from acts.test_utils.tel.tel_defines import GEN_3G
+from acts.test_utils.tel.tel_defines import GEN_4G
+from acts.test_utils.tel.tel_defines import WFC_MODE_WIFI_PREFERRED
+from acts.test_utils.tel.tel_lookup_tables import \
+    network_preference_for_generation
+from acts.test_utils.tel.tel_lookup_tables import operator_capabilities
 from acts.test_utils.tel.tel_test_utils import fastboot_wipe
+from acts.test_utils.tel.tel_test_utils import get_sim_state
 from acts.test_utils.tel.tel_test_utils import is_sim_lock_enabled
+from acts.test_utils.tel.tel_test_utils import is_sim_locked
 from acts.test_utils.tel.tel_test_utils import reboot_device
 from acts.test_utils.tel.tel_test_utils import reset_device_password
 from acts.test_utils.tel.tel_test_utils import refresh_sl4a_session
 from acts.test_utils.tel.tel_test_utils import toggle_airplane_mode_by_adb
 from acts.test_utils.tel.tel_test_utils import unlocking_device
 from acts.test_utils.tel.tel_test_utils import unlock_sim
+from acts.test_utils.tel.tel_voice_utils import phone_setup_iwlan
+from TelLiveEmergencyTest import IMS_FIRST_OFF
 from TelLiveEmergencyTest import TelLiveEmergencyTest
 
 EXPECTED_CALL_TEST_RESULT = False
@@ -37,6 +49,7 @@ EXPECTED_CALL_TEST_RESULT = False
 
 class TelLiveLockedSimTest(TelLiveEmergencyTest):
     def setup_class(self):
+        TelephonyBaseTest.setup_class(self)
         for ad in self.my_devices:
             if not is_sim_lock_enabled(ad):
                 ad.log.info("SIM is not locked")
@@ -51,7 +64,7 @@ class TelLiveLockedSimTest(TelLiveEmergencyTest):
             if not is_sim_lock_enabled(ad):
                 ad.log.info("SIM is not locked")
             else:
-                ad.log.info("SIM is locked")
+                ad.log.info("SIM is locked, set as DUT")
                 self.setup_dut(ad)
                 return True
         self.log.error("There is no locked SIM in this testbed")
@@ -59,9 +72,32 @@ class TelLiveLockedSimTest(TelLiveEmergencyTest):
 
     def setup_test(self):
         self.expected_call_result = False
-        unlocking_device(self.dut)
-        refresh_sl4a_session(self.dut)
-        unlock_sim(self.dut)
+        if "wfc" in self.test_name:
+            if CAPABILITY_WFC not in operator_capabilities.get(
+                    self.dut_operator, operator_capabilities["default"]):
+                raise signals.TestSkip(
+                    "WFC is not supported for carrier %s" % self.dut_operator)
+            unlock_sim(self.dut)
+            if "apm_off" not in self.test_name:
+                if self.dut_operator != "tmo":
+                    raise signals.TestSkip(
+                        "WFC in non-APM is not supported for carrier %s" %
+                        self.dut_operator)
+                if not phone_setup_iwlan(
+                        self.log, self.dut, False, WFC_MODE_WIFI_PREFERRED,
+                        self.wifi_network_ssid, self.wifi_network_pass):
+                    self.dut.log.error("Failed to setup WFC in non-APM.")
+                    return False
+            else:
+                if not phone_setup_iwlan(
+                        self.log, self.dut, True, WFC_MODE_WIFI_PREFERRED,
+                        self.wifi_network_ssid, self.wifi_network_pass):
+                    self.dut.log.error("Failed to setup WFC in APM.")
+                    return False
+        if not is_sim_locked(self.dut):
+            self.dut.reboot(stop_at_lock_screen=True)
+            self.dut.start_services()
+        self.dut.log.info("SIM at state %s", get_sim_state(self.dut))
 
     """ Tests Begin """
 
@@ -79,8 +115,114 @@ class TelLiveLockedSimTest(TelLiveEmergencyTest):
             True if success.
             False if failed.
         """
-        self.expected_call_result = True
         toggle_airplane_mode_by_adb(self.log, self.dut, False)
+        return self.fake_emergency_call_test()
+
+    @test_tracker_info(uuid="a0b3e7dd-93e0-40e2-99a9-5564d34712fc")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_fake_emergency_call_by_emergency_dialer_csfb(self):
+        """Test emergency call with emergency dialer in user account.
+
+        Configure DUT in CSFB
+        Enable SIM lock on the SIM. Reboot device to SIM pin request page.
+        Add system emergency number list with storyline number.
+        Use the emergency dialer to call storyline.
+        Verify DUT has in call activity.
+
+        Returns:
+            True if success.
+            False if failed.
+        """
+        network_preference = network_preference_for_generation(
+            GEN_4G, None, self.dut.droid.telephonyGetPhoneType())
+        self.dut.log.info("Set network preference to %s", network_preference)
+        self.dut.droid.telephonySetPreferredNetworkTypes(network_preference)
+        self.dut.adb.shell(IMS_FIRST_OFF)
+
+        return self.fake_emergency_call_test()
+
+    @test_tracker_info(uuid="b5a5b550-49e6-4026-902a-b155d1209f6d")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_fake_emergency_call_by_emergency_dialer_3g(self):
+        """Test emergency call with emergency dialer in user account.
+
+        Configure DUT in 3G
+        Enable SIM lock on the SIM. Reboot device to SIM pin request page.
+        Add a fake emergency number.
+        Use the emergency dialer to call storyline.
+        Verify DUT has in call activity.
+        Verify DUT in emergency call back mode.
+
+        Returns:
+            True if success.
+            False if failed.
+        """
+        network_preference = network_preference_for_generation(
+            GEN_3G, None, self.dut.droid.telephonyGetPhoneType())
+        self.dut.log.info("Set network preference to %s", network_preference)
+        self.dut.droid.telephonySetPreferredNetworkTypes(network_preference)
+        self.dut.adb.shell(IMS_FIRST_OFF)
+        return self.fake_emergency_call_test()
+
+    @test_tracker_info(uuid="5f083fa7-ddea-44de-8479-4da88d53da65")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_fake_emergency_call_by_emergency_dialer_2g(self):
+        """Test emergency call with emergency dialer in user account.
+
+        Configure DUT in 2G
+        Enable SIM lock on the SIM. Reboot device to SIM pin request page.
+        Add system emergency number list with fake emergency number.
+        Use the emergency dialer to call fake emergency.
+        Verify DUT has in call activity.
+        Verify DUT in emergency call back mode.
+
+        Returns:
+            True if success.
+            False if failed.
+        """
+        network_preference = network_preference_for_generation(
+            GEN_2G, None, self.dut.droid.telephonyGetPhoneType())
+        self.dut.log.info("Set network preference to %s", network_preference)
+        self.dut.droid.telephonySetPreferredNetworkTypes(network_preference)
+        self.dut.adb.shell(IMS_FIRST_OFF)
+        return self.fake_emergency_call_test()
+
+    @test_tracker_info(uuid="e01870d7-89a6-4641-84c6-8e71142773f8")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_fake_emergency_call_by_emergency_dialer_wfc_apm(self):
+        """Test emergency call with emergency dialer in user account.
+
+        Configure DUT in WFC APM on.
+        Enable SIM lock on the SIM. Reboot device to SIM pin request page.
+        Add system emergency number list with fake emergency number.
+        Use the emergency dialer to call storyline.
+        Verify DUT has in call activity.
+        Verify DUT in emergency call back mode.
+
+        Returns:
+            True if success.
+            False if failed.
+        """
+        self.dut.adb.shell(IMS_FIRST_OFF)
+        return self.fake_emergency_call_test()
+
+    @test_tracker_info(uuid="b2c3de31-79ec-457a-a947-50c28caec214")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_fake_emergency_call_by_emergency_dialer_wfc_apm_off(self):
+        """Test emergency call with emergency dialer in user account.
+
+        Configure DUT in WFC APM off.
+        Enable SIM lock on the SIM. Reboot device to SIM pin request page.
+        Add system emergency number list with fake emergency number.
+        Use the emergency dialer to call storyline.
+        Verify DUT has in call activity.
+        Verify DUT in emergency call back mode.
+
+        Returns:
+            True if success.
+            False if failed.
+        """
+        self.dut.adb.shell(IMS_FIRST_OFF)
         return self.fake_emergency_call_test()
 
     @test_tracker_info(uuid="669cf1d9-9513-4f90-b0fd-2f0e8f1cc941")
@@ -89,7 +231,7 @@ class TelLiveLockedSimTest(TelLiveEmergencyTest):
         """Test emergency call with dialer.
 
         Enable SIM lock on the SIM. Reboot device to SIM pin request page.
-        Add system emergency number list with storyline number.
+        Add system emergency number list with fake emergency number.
         Call storyline by dialer.
         Verify DUT has in call activity.
 
@@ -97,7 +239,6 @@ class TelLiveLockedSimTest(TelLiveEmergencyTest):
             True if success.
             False if failed.
         """
-        self.expected_call_result = True
         toggle_airplane_mode_by_adb(self.log, self.dut, False)
         return self.fake_emergency_call_test(by_emergency_dialer=True)
 
@@ -108,7 +249,7 @@ class TelLiveLockedSimTest(TelLiveEmergencyTest):
 
         Enable airplane mode.
         Enable SIM lock on the SIM. Reboot device to SIM pin request page.
-        Add system emergency number list with storyline number.
+        Add system emergency number list with fake emergency number.
         Use the emergency dialer to call storyline.
         Verify DUT has in call activity.
 
@@ -116,7 +257,6 @@ class TelLiveLockedSimTest(TelLiveEmergencyTest):
             True if success.
             False if failed.
         """
-        self.expected_call_result = True
         try:
             toggle_airplane_mode_by_adb(self.log, self.dut, True)
             if self.fake_emergency_call_test():
