126f6fc5612cc9245c8731d5b8b448d03944000d
Etan Cohen <etancohen@google.com>
2018-05-09 12:49:58 -0700

    [ACTS] Disable Location Mode confirmation dialogs
    Bug: 79426050
    Test: AttachTest passes, no pop ups
    Change-Id: I391d6e16e47196c0f7dfd48037499be1eb85fba5

--- acts/framework/acts/utils.py
+++ acts/framework/acts/utils.py
@@ -707,6 +707,12 @@ def set_location_service(ad, new_state):
             If new_state is False, turn off location service.
             If new_state if True, set location service to "High accuracy".
     """
+    ad.adb.shell("adb shell content insert --uri "
+                 " content://com.google.settings/partner --bind "
+                 "name:s:network_location_opt_in --bind value:s:1")
+    ad.adb.shell("adb shell content insert --uri "
+                 " content://com.google.settings/partner --bind "
+                 "name:s:use_location_for_services --bind value:s:1")
     if new_state:
         ad.adb.shell("settings put secure location_providers_allowed +gps")
         ad.adb.shell("settings put secure location_providers_allowed +network")
