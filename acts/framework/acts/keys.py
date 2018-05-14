b9f6f18b053fdb4ea811e089fc93894ff38f919e
Girish Moturu <gmoturu@google.com>
2018-05-14 15:56:41 -0700

    [wifi_controller.py] Add new wifi controller
    Added wifi_controller.py to handle arduino wifi controller devices.
    Also, added connect_wifi.ino which includes sending and receiving UDP
    packets and disconnect_wifi.ino which includes scan networks from wifi
    controller
    
    Change-Id: I21888fad503e5abf943adf9a0590d8e12e683a4c

--- acts/framework/acts/keys.py
+++ acts/framework/acts/keys.py
@@ -51,6 +51,7 @@ class Config(enum.Enum):
     key_packet_sender = "PacketSender"
     key_monsoon = "Monsoon"
     key_sniffer = "Sniffer"
+    key_arduino_wifi_dongle = "ArduinoWifiDongle"
     # Internal keys, used internally, not exposed to user's config files.
     ikey_user_param = "user_params"
     ikey_testbed_name = "testbed_name"
@@ -69,6 +70,7 @@ class Config(enum.Enum):
     m_key_iperf_client = "iperf_client"
     m_key_packet_sender = "packet_sender"
     m_key_sniffer = "sniffer"
+    m_key_arduino_wifi_dongle = "arduino_wifi_dongle"
 
     # A list of keys whose values in configs should not be passed to test
     # classes without unpacking first.
@@ -87,6 +89,7 @@ class Config(enum.Enum):
         key_monsoon,
         key_sniffer,
         key_chameleon_device,
+        key_arduino_wifi_dongle,
     ]
 
     # Keys that are file or folder paths.
