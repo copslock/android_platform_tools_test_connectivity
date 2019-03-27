3492edd57b018d2a6cd74520a0701612f6443144
Hsiu-Chang Chen <hsiuchangchen@google.com>
2019-03-27 10:28:58 +0800

    Disable device password with correct password
    Current disableDevicePassword calls in WifiEnterpriseTest and
    WifiEnterpriseRoamingTest will be failure because the changes
    of LockPatternUtils.
    
    Bug: 129369317
    Test: WifiEnterpriseTest, WifiEnterpriseRoamingTest
    Change-Id: If4a42bbbc6a0ba7e8b268fa3013c5fa37778f340

--- acts/tests/google/wifi/WifiEnterpriseRoamingTest.py
+++ acts/tests/google/wifi/WifiEnterpriseRoamingTest.py
@@ -104,7 +104,7 @@ class WifiEnterpriseRoamingTest(WifiBaseTest):
 
     def teardown_class(self):
         wutils.reset_wifi(self.dut)
-        self.dut.droid.disableDevicePassword()
+        self.dut.droid.disableDevicePassword(self.device_password)
         self.dut.ed.clear_all_events()
         self.set_attns("default")
 
