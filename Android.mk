06968f5ebb8da60857b16ec94e9f331647ef9ca9
Dan Willemsen <dwillemsen@google.com>
2019-05-06 13:54:47 -0700

    Allow specifying acts w/dist
    Instead of requiring users to build all tests in the tree in order to
    get this test suite into the dist files, add the custom phony targets to
    dist-for-goals.
    
    Bug: 132082642
    Bug: 132098164
    Test: m acts dist
    Change-Id: I2e8b8cb6f3fc3329f0b76d5679ce4ba07808031a
    Merged-In: I2e8b8cb6f3fc3329f0b76d5679ce4ba07808031a
    (cherry picked from commit c59a9c624abff3b1ba1d1c80efde62445f713f7b)

--- Android.mk
+++ Android.mk
@@ -29,6 +29,6 @@ $(ACTS_DISTRO): $(sort $(shell find $(LOCAL_PATH)/acts))
 	$(hide) zip $(HOST_OUT)/acts-dist/acts.zip $(shell find tools/test/connectivity/acts/* ! -wholename "*__pycache__*")
 acts: $(ACTS_DISTRO)
 
-$(call dist-for-goals,tests,$(ACTS_DISTRO))
+$(call dist-for-goals,acts tests,$(ACTS_DISTRO))
 
 endif
