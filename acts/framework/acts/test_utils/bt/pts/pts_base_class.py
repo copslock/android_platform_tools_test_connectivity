29eba704ddc6ccc498b88b999eb8da155e3a2707
tturney <tturney@google.com>
2019-07-19 09:03:28 -0700

    Add tests for PTS SDP automation.
    Bug: None
    Test: SdpPtsTest
    Change-Id: Id210e1646c82bf12e2a5c4a7aff6d0068799c667

--- acts/framework/acts/test_utils/bt/pts/pts_base_class.py
+++ acts/framework/acts/test_utils/bt/pts/pts_base_class.py
@@ -116,6 +116,9 @@ class PtsBaseClass(BaseTestClass):
                 119: self.mmi_iut_enter_uuid_attribute_not_found,
                 120: self.mmi_iut_enter_handle_write_not_permitted,
                 2000: self.mmi_verify_secure_id,  # Enter pairing pin from DUT.
+            },
+            "SDP" {
+                # TODO: Implement MMIs as necessary
             }
         }
 
