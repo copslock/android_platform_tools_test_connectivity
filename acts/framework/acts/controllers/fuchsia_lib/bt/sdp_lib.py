a0cd1fa43b7961994bbce79daa2d6cc4f736b4e0
git status <tturney@google.com>
2019-08-07 13:55:33 -0700

    Add l2cap_connect api to sdp_lib
    Bug: None
    Test: PTS l2cap tests
    Change-Id: I3e4ba07b9ec4063f3235029ee930d3727488b998

--- acts/framework/acts/controllers/fuchsia_lib/bt/sdp_lib.py
+++ acts/framework/acts/controllers/fuchsia_lib/bt/sdp_lib.py
@@ -18,7 +18,6 @@ from acts.controllers.fuchsia_lib.base_lib import BaseLib
 
 
 class FuchsiaProfileServerLib(BaseLib):
-
     def __init__(self, addr, tc, client_id):
         self.address = addr
         self.test_counter = tc
@@ -104,6 +103,43 @@ class FuchsiaProfileServerLib(BaseLib):
         """
         test_cmd = "profile_server_facade.ProfileServerCleanup"
         test_args = {}
+        test_id = self.build_id(self.test_counter)
+        self.test_counter += 1
+
+        return self.send_command(test_id, test_cmd, test_args)
+
+    def connectL2cap(self, identifier, psm):
+        """ Sends an outgoing l2cap connection to a connected peer device.
+
+        Args:
+            psm: The psm value to connect over. Available PSMs:
+                SDP 0x0001  See Bluetooth Service Discovery Protocol (SDP)
+                RFCOMM  0x0003  See RFCOMM with TS 07.10
+                TCS-BIN 0x0005  See Bluetooth Telephony Control Specification /
+                    TCS Binary
+                TCS-BIN-CORDLESS    0x0007  See Bluetooth Telephony Control
+                    Specification / TCS Binary
+                BNEP    0x000F  See Bluetooth Network Encapsulation Protocol
+                HID_Control 0x0011  See Human Interface Device
+                HID_Interrupt   0x0013  See Human Interface Device
+                UPnP    0x0015  See [ESDP]
+                AVCTP   0x0017  See Audio/Video Control Transport Protocol
+                AVDTP   0x0019  See Audio/Video Distribution Transport Protocol
+                AVCTP_Browsing  0x001B  See Audio/Video Remote Control Profile
+                UDI_C-Plane 0x001D  See the Unrestricted Digital Information
+                    Profile [UDI]
+                ATT 0x001F  See Bluetooth Core Specification​
+                ​3DSP   0x0021​ ​​See 3D Synchronization Profile.
+                ​LE_PSM_IPSP    ​0x0023 ​See Internet Protocol Support Profile
+                    (IPSP)
+                OTS 0x0025  See Object Transfer Service (OTS)
+                EATT    0x0027  See Bluetooth Core Specification
+
+        Returns:
+            Dictionary, None if success, error if error.
+        """
+        test_cmd = "profile_server_facade.ProfileServerConnectL2cap"
+        test_args = {"identifier": identifier, "psm": psm}
         test_id = self.build_id(self.test_counter)
         self.test_counter += 1
 
