5b19a12c555bbc004a6759ade5afcf1df4b621ad
Roshan Pius <rpius@google.com>
2019-09-13 08:07:30 -0700

    ACTS(wifi): Use shell command for setting country code
    WifiManager.setCountryCode() is being removed, use the shell command to
    set country code instead from ACTS tests.
    
    Bug: 141107015
    Test: Verified the changes
    Change-Id: I23ebe57fcb8bed71b6b85ec1697e80bf150044f2
    Merged-In: I23ebe57fcb8bed71b6b85ec1697e80bf150044f2
    (cherry picked from commit 48df08cf90347b2419987942dd2f26bcd903585e)

--- acts/tests/google/wifi/WifiPingTest.py
+++ acts/tests/google/wifi/WifiPingTest.py
@@ -420,7 +420,7 @@ class WifiPingTest(base_test.BaseTestClass):
             self.log.info('Already connected to desired network')
         else:
             wutils.reset_wifi(self.dut)
-            self.dut.droid.wifiSetCountryCode(
+            wutils.set_wifi_country_code(self.dut,
                 self.testclass_params['country_code'])
             testcase_params['test_network']['channel'] = testcase_params[
                 'channel']
