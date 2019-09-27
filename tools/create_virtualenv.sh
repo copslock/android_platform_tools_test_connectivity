6e5a5d5a8f70f99d6cd60b3e80e8c76445be6cbe
Mark De Ruyter <markdr@google.com>
2019-09-27 11:01:38 -0700

    Running upload installs virtualenv if not already installed.
    Bug: None
    Test: manual
    Change-Id: Ie8c1786d4fbb172b69d5ff1035c9337584610a62

--- tools/create_virtualenv.sh
+++ tools/create_virtualenv.sh
@@ -2,7 +2,8 @@
 
 virtualenv='/tmp/acts_preupload_virtualenv'
 
-python3 -m virtualenv $virtualenv
+python3 -m pip install virtualenv
+python3 -m virtualenv -p python3 $virtualenv
 cp -r acts/framework $virtualenv/
 cd $virtualenv/framework
 $virtualenv/bin/python3 setup.py develop
