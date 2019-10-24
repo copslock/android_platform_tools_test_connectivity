a22dbafe554a5e8926dd2537aef9eb3fe37cb568
Hayden Nix <haydennix@google.com>
2019-10-24 15:48:06 -0700

    Add mock of Asus RTAC5300 AP.
    This CL adds a profile to create a mock Asus RTAC5300 AP.
    
    Bug: None
    Test: Compared simulated beacon and probe response to actual.
    Change-Id: I57c4f6665718c4865855941b4170da40d55f2218

--- acts/framework/acts/controllers/ap_lib/third_party_ap_profiles/asus.py
+++ acts/framework/acts/controllers/ap_lib/third_party_ap_profiles/asus.py
@@ -259,3 +259,137 @@ def asus_rtac86u(iface_wlan_2g=None,
         spectrum_mgmt_required=spectrum_mgmt,
         additional_parameters=additional_params)
     return config
+
+
+def asus_rtac5300(iface_wlan_2g=None,
+                  iface_wlan_5g=None,
+                  channel=None,
+                  security=None,
+                  ssid=None):
+    # TODO(b/143104825): Permit RIFS once it is supported
+    """A simulated implementation of an Asus RTAC5300 AP.
+    Args:
+        iface_wlan_2g: The 2.4Ghz interface of the test AP.
+        iface_wlan_5g: The 5Ghz interface of the test AP.
+        channel: What channel to use.
+        security: A security profile.  Must be none or WPA2 as this is what is
+            supported by the RTAC5300.
+        ssid: Network name
+    Returns:
+        A hostapd config
+    Differences from real RTAC5300:
+        2.4GHz:
+            Rates:
+                RTAC86U:
+                    Supported: 1, 2, 5.5, 11, 18, 24, 36, 54
+                    Extended: 6, 9, 12, 48
+                Simulated:
+                    Supported: 1, 2, 5.5, 11, 6, 9, 12, 18
+                    Extended: 24, 36, 48, 54
+        5GHz:
+            VHT Capab:
+                RTAC5300:
+                    SU Beamformer Supported,
+                    SU Beamformee Supported,
+                    Beamformee STS Capability: 4,
+                    Number of Sounding Dimensions: 4,
+                    MU Beamformer Supported,
+                    VHT Link Adaptation: Both
+                Simulated:
+                    Above are not supported by driver
+            VHT Operation Info:
+                RTAC5300: Basic MCS Map (0x0000)
+                Simulated: Basic MCS Map (0xfffc)
+            VHT Tx Power Envelope:
+                RTAC5300: Local Max Tx Pwr Constraint: 1.0 dBm
+                Simulated: Local Max Tx Pwr Constraint: 23.0 dBm
+        Both:
+            HT Capab:
+                A-MPDU
+                    RTAC5300: MPDU Density 4
+                    Simulated: MPDU Density 8
+            HT Info:
+                RTAC5300: RIFS Permitted
+                Simulated: RIFS Prohibited
+    """
+    if not iface_wlan_2g or not iface_wlan_5g:
+        raise ValueError('Wlan interface for 2G and/or 5G is missing.')
+    if (iface_wlan_2g not in hostapd_constants.INTERFACE_2G_LIST
+            or iface_wlan_5g not in hostapd_constants.INTERFACE_5G_LIST):
+        raise ValueError('Invalid interface name was passed.')
+    if security:
+        if security.security_mode is hostapd_constants.WPA2:
+            if not security.wpa2_cipher == 'CCMP':
+                raise ValueError('The mock ASUS RTAC5300 only supports a WPA2 '
+                                 'unicast and multicast cipher of CCMP. '
+                                 'Invalid cipher mode (%s)' %
+                                 security.security.wpa2_cipher)
+        else:
+            raise ValueError(
+                'The Asus RTAC5300 only supports WPA2 or open. Invalid '
+                'security mode (%s)' % security.security_mode)
+
+    # Common Parameters
+    rates = {'supported_rates': '10 20 55 110 60 90 120 180 240 360 480 540'}
+    qbss = {'bss_load_update_period': 50, 'chan_util_avg_period': 600}
+    n_capabilities = [
+        hostapd_constants.N_CAPABILITY_LDPC,
+        hostapd_constants.N_CAPABILITY_TX_STBC,
+        hostapd_constants.N_CAPABILITY_RX_STBC1,
+        hostapd_constants.N_CAPABILITY_SGI20
+    ]
+    # Broadcom IE
+    vendor_elements = {'vendor_elements': 'dd090010180200009c0000'}
+
+    # 2.4GHz
+    if channel <= 11:
+        interface = iface_wlan_2g
+        rates['basic_rates'] = '10 20 55 110'
+        mode = hostapd_constants.MODE_11N_MIXED
+        # AsusTek IE
+        # Epigram 2.4GHz IE
+        vendor_elements['vendor_elements'] += 'dd25f832e4010101020100031411b5' \
+        '2fd437509c30b3d7f5cf5754fb125aed3b8507045aed3b85' \
+        'dd1e00904c0418bf0cb2798b0faaff0000aaff0000c0050001000000c3020002'
+        ac_capabilities = None
+        vht_channel_width = None
+        vht_center_channel = None
+
+    # 5GHz
+    else:
+        interface = iface_wlan_5g
+        rates['basic_rates'] = '60 120 240'
+        mode = hostapd_constants.MODE_11AC_MIXED
+        # Epigram 5GHz IE
+        vendor_elements['vendor_elements'] += 'dd0500904c0410'
+        ac_capabilities = [
+            hostapd_constants.AC_CAPABILITY_RXLDPC,
+            hostapd_constants.AC_CAPABILITY_SHORT_GI_80,
+            hostapd_constants.AC_CAPABILITY_TX_STBC_2BY1,
+            hostapd_constants.AC_CAPABILITY_RX_STBC_1,
+            hostapd_constants.AC_CAPABILITY_MAX_MPDU_11454,
+            hostapd_constants.AC_CAPABILITY_MAX_A_MPDU_LEN_EXP7
+        ]
+        vht_channel_width = 40
+        vht_center_channel = 36
+
+    additional_params = _merge_dicts(rates, qbss, vendor_elements,
+                                     hostapd_constants.UAPSD_ENABLED)
+
+    config = hostapd_config.HostapdConfig(
+        ssid=ssid,
+        channel=channel,
+        hidden=False,
+        security=security,
+        interface=interface,
+        mode=mode,
+        force_wmm=True,
+        beacon_interval=100,
+        dtim_period=3,
+        short_preamble=False,
+        n_capabilities=n_capabilities,
+        ac_capabilities=ac_capabilities,
+        vht_channel_width=vht_channel_width,
+        vht_center_channel=vht_center_channel,
+        additional_parameters=additional_params)
+    return config
