a70b22481bcf0b10b7589ac07896bb695d0f9fe9
sairam <ganeshganesh@google.com>
2020-01-02 22:19:47 -0800

    Make changes to run pixel as a2dp sink.
    This CL also removes the deprecated apollo tests.
    
    Bug: None
    Change-Id: I5e68cac76fec658dbeada9643ad40864793e49e5

--- acts/framework/acts/test_utils/coex/CoexPerformanceBaseTest.py
+++ acts/framework/acts/test_utils/coex/CoexPerformanceBaseTest.py
@@ -21,6 +21,8 @@ from collections import defaultdict
 
 from acts.metrics.loggers.blackbox import BlackboxMetricLogger
 from acts.test_utils.bt.bt_test_utils import disable_bluetooth
+from acts.test_utils.coex.audio_test_utils import AudioCaptureResult
+from acts.test_utils.coex.audio_test_utils import get_audio_capture_device
 from acts.test_utils.coex.CoexBaseTest import CoexBaseTest
 from acts.test_utils.coex.coex_test_utils import bokeh_chart_plot
 from acts.test_utils.coex.coex_test_utils import collect_bluetooth_manager_dumpsys_logs
@@ -30,8 +32,8 @@ from acts.test_utils.wifi.wifi_test_utils import wifi_connect
 from acts.test_utils.wifi.wifi_test_utils import wifi_test_device_init
 from acts.utils import get_current_epoch_time
 
-RSSI_POLL_RESULTS = "Monitoring , Handle: 0x0003, POLL"
-RSSI_RESULTS = "Monitoring , Handle: 0x0003, "
+RSSI_POLL_RESULTS = 'Monitoring , Handle: 0x0003, POLL'
+RSSI_RESULTS = 'Monitoring , Handle: 0x0003, '
 
 
 def get_atten_range(start, stop, step):
@@ -70,12 +72,13 @@ class CoexPerformanceBaseTest(CoexBaseTest):
             metric_name='wifi_range_metric')
 
     def setup_class(self):
-        req_params = ["test_params", "Attenuator"]
-        self.unpack_userparams(req_params)
-        if hasattr(self, "Attenuator"):
+        req_params = ['test_params', 'Attenuator']
+        opt_params = ['audio_params']
+        self.unpack_userparams(req_params, opt_params)
+        if hasattr(self, 'Attenuator'):
             self.num_atten = self.attenuators[0].instrument.num_atten
         else:
-            self.log.error("Attenuator should be connected to run tests.")
+            self.log.error('Attenuator should be connected to run tests.')
             return False
         for i in range(self.num_atten):
             self.attenuators[i].set_atten(0)
@@ -97,7 +100,9 @@ class CoexPerformanceBaseTest(CoexBaseTest):
                             self.test_params["attenuation_step"]))
 
     def setup_test(self):
-        if "a2dp_streaming" in self.current_test_name:
+        if ('a2dp_streaming' in self.current_test_name and
+                hasattr(self, 'audio_params')):
+            self.audio = get_audio_capture_device(self.sec_ad, self.audio_params)
             self.a2dp_streaming = True
         for i in range(self.num_atten):
             self.attenuators[i].set_atten(0)
@@ -212,6 +217,8 @@ class CoexPerformanceBaseTest(CoexBaseTest):
                 return self.iperf_received, self.a2dp_dropped_list, False
             time.sleep(5)  # Time for attenuation to set.
             begin_time = get_current_epoch_time()
+            if self.a2dp_streaming:
+                self.audio.start()
             if called_func:
                 if not multithread_func(self.log, called_func):
                     self.iperf_received.append(float(str(
@@ -230,7 +237,9 @@ class CoexPerformanceBaseTest(CoexBaseTest):
                 self.log.info('Android device: {}'.format((
                     adb_rssi_results[-1]['log_message']).split(',')[5]))
             if self.a2dp_streaming:
-                analysis_path = self.audio.audio_quality_analysis(self.log_path)
+                self.path = self.audio.stop()
+                analysis_path = AudioCaptureResult(
+                    self.path).audio_quality_analysis(self.audio_params)
                 with open(analysis_path) as f:
                     self.rvr[bt_atten]["audio_artifacts"][atten] = f.readline()
                 content = json.loads(self.rvr[bt_atten]["audio_artifacts"][atten])
